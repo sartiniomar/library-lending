662063702d53244bd473241e4ec3be79f818f0f3
sartiniomar <sartiniomar@gmail.com>
2026-09-07 10:49:42 -0300

    OMAR-28 Implement Checkout From Reserve API

--- src/main/java/com/sartiniomar/library/loan/infrastructure/config/UseCaseConfig.java
+++ src/main/java/com/sartiniomar/library/loan/infrastructure/config/UseCaseConfig.java
@@ -1,16 +1,19 @@
 package com.sartiniomar.library.loan.infrastructure.config;
 
 import com.sartiniomar.library.loan.application.port.in.CancelUseCase;
+import com.sartiniomar.library.loan.application.port.in.CheckoutReserveUseCase;
 import com.sartiniomar.library.loan.application.port.in.CheckoutUseCase;
 import com.sartiniomar.library.loan.application.port.in.ReserveUseCase;
 import com.sartiniomar.library.loan.application.port.out.BookInstanceLoanRepository;
 import com.sartiniomar.library.loan.application.port.out.LoanRepository;
 import com.sartiniomar.library.loan.application.port.out.PatronLoanRepository;
 import com.sartiniomar.library.loan.application.usecase.CancelUseCaseImpl;
+import com.sartiniomar.library.loan.application.usecase.CheckoutReserveUseCaseImpl;
 import com.sartiniomar.library.loan.application.usecase.CheckoutUseCaseImpl;
 import com.sartiniomar.library.loan.application.usecase.ReserveUseCaseImpl;
 import com.sartiniomar.library.loan.application.service.LoanLimitChecker;
 import com.sartiniomar.library.loan.domain.loan.service.CancelServiceDomain;
+import com.sartiniomar.library.loan.domain.loan.service.CheckoutReserveServiceDomain;
 import com.sartiniomar.library.loan.domain.loan.service.CheckoutServiceDomain;
 import com.sartiniomar.library.loan.domain.loan.service.ReserveServiceDomain;
 import org.springframework.context.annotation.Bean;
@@ -63,6 +66,21 @@ public class UseCaseConfig {
         loanRepository,
         service,
         validationsUtil
+    );
+  }
+
+  @Bean
+  CheckoutReserveUseCase checkoutReserveUseCase(
+      LoanRepository loanRepository,
+      PatronLoanRepository patronLoanRepository,
+      BookInstanceLoanRepository bookInstanceLoanRepository,
+      CheckoutReserveServiceDomain service
+  ) {
+    return new CheckoutReserveUseCaseImpl(
+        loanRepository,
+        patronLoanRepository,
+        bookInstanceLoanRepository,
+        service
     );
   }
 }
